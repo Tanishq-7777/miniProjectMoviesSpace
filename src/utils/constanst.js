fd0e21fe8134b8175488a10d83a392a812f9a9a6
Tani-7777 <saxenatanishq4567@gmail.com>
2026-04-12 17:21:54 +0530

    some minor chnages

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -1,3 +1,5 @@
 // const BASE_URL = "http://localhost:7777/";
-const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:7777/";
+
+const BASE_URL =
+  "https://moviespacedb.onrender.com/" || "http://localhost:7777/";
 export default BASE_URL;
